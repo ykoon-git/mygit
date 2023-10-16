025f56619dc12dd4d002dd08dfb4f974c96e2b5a
koonyoun <koonyoun@amazon.com>
2023-10-16 21:22:14 +0900

    add grado test

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
 import pandas as pd
-import seaborn as sns
 import numpy as np
+import seaborn as sns
 
 df = pd.read_csv("https://raw.githubusercontent.com/noahgift/regression-concepts/master/height-weight-25k.csv")
 
 
-
-sns.lmplot(x="Height", y="Weight", data=df)
-sns.lmplot(x="Height", y="Weight", data=df, hue="Gender")
+print (df.head())
+print (df.describe())
 
