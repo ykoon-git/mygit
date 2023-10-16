8ebae82951b42a7168225f235932d1f89dcb4604
koonyoun <koonyoun@amazon.com>
2023-10-16 23:04:19 +0900

    add pytorch mnist

--- gradio-test.py
+++ gradio-test.py
@@ -1,8 +1,12 @@
 import gradio as gr
+import torch
 
 def greet(name):
     return "Hello " + name + "!"
 
 iface = gr.Interface(fn=greet, inputs="text", outputs="text")
 iface.launch()
+
+x = torch.rand(5, 3)
+print(x)
 
